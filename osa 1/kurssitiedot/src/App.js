a2fa51f99565362efab32c826e7a84bfd43a5bd7
prpkr <56441900+prpkr@users.noreply.github.com>
2021-05-11 08:09:44 +0300

    kurssitiedot 1.5
    Käytetään olioita

--- osa 1/kurssitiedot/src/App.js
+++ osa 1/kurssitiedot/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Header = (props) => {
+  
   return (
-    <h1>{props.header}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
 const Part = (props) => {
+  
   return (
     <div>
       <p>
@@ -19,38 +21,46 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+      <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+      <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.course.parts[0].exercises+props.course.parts[1].exercises+props.course.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       
-      <Header header={course} />
+      <Header course={course} />
 
-      // laitetaan Content komponentin sisältyville Part komponenteille propsit tässä
+      <Content course={course} />
 
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total course={course} />
 
       
     </div>
